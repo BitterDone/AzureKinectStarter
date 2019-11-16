152de6feb17742b9be40c2edf20069113c3e0fba
Hololens Dev Pc <bitter.daniel@gmail.com>
2019-11-16 17:45:39 -0600

    Broke CameraViewingPlane and DebugRenderer to own GameObjects. DR still manages everything. Also fixed toggle button color status to reflect on/off

--- Assets/Scripts/DebugRenderer.cs
+++ Assets/Scripts/DebugRenderer.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using Microsoft.Azure.Kinect.Sensor;
 using Microsoft.Azure.Kinect.Sensor.BodyTracking;
 using Stahle.Utility;
-using APRLM.Game;
 using UnityEngine.UI;
 using System.Collections;
 
@@ -15,31 +14,52 @@ public class DebugRenderer : PersistantSingleton<DebugRenderer>
     [SerializeField] GameObject[] blockmanArray;
     public Toggle recordPoseToggle; //dragged in manually 
     public Renderer renderer;
-    Skeleton skeleton;
+	public GameObject blockPrefab;
+	Skeleton skeleton;
 #if UNITY_EDITOR_WIN
     Device device;
     BodyTracker tracker;
 #endif
 
     protected override void Awake()
-    {
-        base.Awake();
-    }
+	{
+		Screen.sleepTimeout = SleepTimeout.NeverSleep;
+		base.Awake();
+		MakeBlockMan();
+	}
 
 	public void Start()
 	{
-		blockmanArray = GameManager.Instance.blockman;
 		foreach (GameObject go in blockmanArray)
 		{
 			go.SetActive(true);
 		}
+		recordPoseToggle.onValueChanged.AddListener(OnToggleValueChanged);
 #if UNITY_EDITOR_WIN
-        InitCamera();
+		InitCamera();
 #endif
     }
 
+	void MakeBlockMan()
+	{
+		int numberOfJoints = (int)JointId.Count;
+
+		blockmanArray = new GameObject[numberOfJoints];
+
+		for (var i = 0; i < numberOfJoints; i++)
+		{
+			GameObject jointCube = Instantiate(blockPrefab, transform);
+			//deactivate it - (its Start() or OnEnable() won't be called)
+			jointCube.SetActive(false);
+			jointCube.name = Enum.GetName(typeof(JointId), i);
+			//why do we multiply by .4?  idk
+			jointCube.transform.localScale = Vector3.one * 0.4f;
+			blockmanArray[i] = jointCube;
+		}
+	}
+
 #if UNITY_EDITOR_WIN
-    void InitCamera()
+	void InitCamera()
     {
         this.device = Device.Open(0);
 		var config = new DeviceConfiguration
@@ -52,8 +72,10 @@ public class DebugRenderer : PersistantSingleton<DebugRenderer>
 
         var calibration = device.GetCalibration(config.DepthMode, config.ColorResolution);
         this.tracker = BodyTracker.Create(calibration);
-        renderer = GetComponent<Renderer>();
-    }
+
+		GameObject cameraFeed = GameObject.FindWithTag("CameraFeed");
+		renderer = cameraFeed.GetComponent<Renderer>();
+	}
 #endif
 
     void Update()
@@ -122,6 +144,24 @@ public class DebugRenderer : PersistantSingleton<DebugRenderer>
 		canUpdate = !canUpdate;
     }
 
+	void OnToggleValueChanged(bool isOn)
+	{
+		ColorBlock cb = recordPoseToggle.colors;
+		cb.normalColor = Color.white; // blue;
+		cb.highlightedColor = Color.white; // green;
+
+		cb.selectedColor = Color.white;
+		cb.pressedColor = Color.red;
+
+		if (isOn)
+		{
+			cb.selectedColor = Color.red;
+			cb.pressedColor = Color.white;
+		}
+
+		recordPoseToggle.colors = cb;
+	}
+
 	void ClearSkeletonsList()
 	{
 		skeletons.Clear();
@@ -132,7 +172,7 @@ public class DebugRenderer : PersistantSingleton<DebugRenderer>
     {
         //todo test if only called once at the end of the program, if so, renable the below
         print("DebugRenderer onDisable was called");
-		device.StopCameras();
+		//device.StopCameras();
 		//k4a_device_close(device) here.
 		if (tracker != null)
 		{
@@ -140,6 +180,7 @@ public class DebugRenderer : PersistantSingleton<DebugRenderer>
 		}
 		if (device != null)
 		{
+			device.StopCameras();
 			device.Dispose();
 		}
 	}
